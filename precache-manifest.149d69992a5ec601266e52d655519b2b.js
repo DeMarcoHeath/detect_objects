2f4d5e5316b2a9930b4b608d93e7f3b8a65897bf
Demarco Heath <demarcoheath@gmail.com>
2021-01-16 02:33:27 -0500

    Updates

---
+++ precache-manifest.149d69992a5ec601266e52d655519b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6a9a2bab863abd6368e7f866627f02",
+    "url": "/DeMarcoHeath/detect_objects/index.html"
+  },
+  {
+    "revision": "581c328a7e88cce6e02b",
+    "url": "/DeMarcoHeath/detect_objects/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "954b454146fddb35db16",
+    "url": "/DeMarcoHeath/detect_objects/static/js/2.41aaaa6e.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/DeMarcoHeath/detect_objects/static/js/2.41aaaa6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "581c328a7e88cce6e02b",
+    "url": "/DeMarcoHeath/detect_objects/static/js/main.03475df5.chunk.js"
+  },
+  {
+    "revision": "73dadb1bf5c7786e4f65",
+    "url": "/DeMarcoHeath/detect_objects/static/js/runtime-main.74d53196.js"
+  }
+]);
